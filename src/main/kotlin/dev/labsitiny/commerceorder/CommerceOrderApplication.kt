e7e45592e41a18d664664945e2e1a7f656adf9b8
Parkseongheum <ab6449@gmail.com>
2022-11-27 19:28:07 +0900

    Init: Order Service

---
+++ src/main/kotlin/dev/labsitiny/commerceorder/CommerceOrderApplication.kt
@@ -0,0 +1,11 @@
+package dev.labsitiny.commerceorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommerceOrderApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommerceOrderApplication>(*args)
+}
---
+++ src/test/kotlin/dev/labsitiny/commerceorder/CommerceOrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.labsitiny.commerceorder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommerceOrderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
