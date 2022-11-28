42768733cc5ee5190ea830472cf2a7c6cbb6ec39
Parkseongheum <ab6449@gmail.com>
2022-11-28 22:09:06 +0900

    Feature: Query, Command 컨트롤러 분리

---
+++ src/main/kotlin/dev/labsitiny/commerceorder/port/rest/OrderQueryController.java
@@ -0,0 +1,10 @@
+package dev.labsitiny.commerceorder.port.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/v1/order/query/")
+public class OrderQueryController {
+
+}
